0fe3125dd57fb7683aaefb61c5eb4838bf5957c5
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-07-23 12:23:22 +0300

    copy

---
+++ LSAT/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 252, green: 252, blue: 252)
+    
+    static let mainColor = UIColor(integralRed: 88, green: 61, blue: 97)
+    static let mainColorAlpha = UIColor(integralRed: 88, green: 61, blue: 97, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 245, green: 174, blue: 120)
+    
+    static let progress1Color = UIColor(integralRed: 88, green: 61, blue: 97)
+    static let progress2Color = UIColor(integralRed: 245, green: 174, blue: 120)
+    static let progress3Color = UIColor(integralRed: 213, green: 77, blue: 91)
+}
